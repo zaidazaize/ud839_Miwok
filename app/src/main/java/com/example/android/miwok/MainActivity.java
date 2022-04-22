2458a01caeba6840a7356a7c5cfdad392b263ed4
zaidazaize <zaid.azaize@gmail.com>
2022-04-23 03:54:30 +0530

    intents working

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -15,7 +15,9 @@
  */
 package com.example.android.miwok;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 // import android.support.v7.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -27,5 +29,26 @@ public class MainActivity extends AppCompatActivity {
 
         // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
+    }
+
+    public void numbersActivity(View view) {
+        Intent intent = new Intent(this, NumberActivity.class);
+        startActivity(intent);
+    }
+
+    public void familyActivity(View view) {
+        Intent intent = new Intent(this, FamilyActivity.class);
+        startActivity(intent);
+    }
+
+    public void colorsActivity(View view) {
+        Intent intent = new Intent(this, ColorsActivity.class);
+        startActivity(intent);
+
+    }
+
+    public void phrasesActivity(View view) {
+        Intent intent = new Intent(this, PhrasesActivity.class);
+        startActivity(intent);
     }
 }
