1e29b59014f9384ea7df576ec186a7fa3c537b6e
zaidazaize <zaid.azaize@gmail.com>
2022-04-30 04:17:34 +0530

    implements lists in PhrasesActivity, FamilyActivity, ColorsActivity

--- app/src/main/java/com/example/android/miwok/ColorsActivity.java
+++ app/src/main/java/com/example/android/miwok/ColorsActivity.java
@@ -1,14 +1,30 @@
 package com.example.android.miwok;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+
+import com.example.android.miwok.adapter.NumberAdapter;
+import com.example.android.miwok.dataclass.LangData;
+
+import java.util.ArrayList;
 
 public class ColorsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_colors);
+        setContentView(R.layout.activity_number);
+
+        //create a list of word
+        ArrayList<LangData> dataSourse = new ArrayList<>();
+        for(int i=0;i<10;i++){
+            dataSourse.add(new LangData(Integer.toString(i)));
+        }
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.numbersrecycleview);
+        recyclerView.setAdapter(new NumberAdapter(dataSourse));
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
